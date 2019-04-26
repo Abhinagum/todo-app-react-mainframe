0763517a4192bd0d613501b14e055826cd969c26
Abhinag <abhinag6@gmail.com>
2019-04-26 17:18:46 +0530

    Updates

---
+++ precache-manifest.42224cdc5d0058a997a7c968d040159c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ed63381bb10a3952e6a",
+    "url": "/todo-app-react/static/js/runtime~main.ec7f1ccf.js"
+  },
+  {
+    "revision": "5381262783b0307b736d",
+    "url": "/todo-app-react/static/js/main.54da7a04.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/todo-app-react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5381262783b0307b736d",
+    "url": "/todo-app-react/static/css/main.2e2626da.chunk.css"
+  },
+  {
+    "revision": "4950519295468286922281831a3a79bd",
+    "url": "/todo-app-react/index.html"
+  }
+];
